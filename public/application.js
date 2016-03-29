478650f1d449c905d7e58b3f71aca6d54be69c86
Seth Anderson <seth.c.anderson@gmail.com>
2016-03-28 22:32:52 -0500

    heroku

--- public/application.js
+++ public/application.js
@@ -1,8 +1,10 @@
 var mainApplicationModuleName = 'mean';
-
+ 
 var mainApplicationModule = angular.module(mainApplicationModuleName, []);
-
-angular.element(document)ready(function() {
+ 
+// Bind a function to the document ready event and use bootstrap to start
+// a new AngularJS app using the main module
+angular.element(document).ready(function() {
     angular.bootstrap(document,
                       [mainApplicationModuleName]);
 });
