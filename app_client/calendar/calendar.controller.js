28142c3c4d0e1728acae49cec118e03f2618e13e
Seth Anderson <seth.c.anderson@gmail.com>
2016-07-12 02:30:44 -0500

    testing timezone change

--- app_client/calendar/calendar.controller.js
+++ app_client/calendar/calendar.controller.js
@@ -23,12 +23,13 @@
     $('#datetimepicker1')
         .datetimepicker()
         .on("dp.change", function() {
-            vm.event.start = moment.utc($("#datetimepicker1").data("DateTimePicker").date()).valueOf();
-            //vm.event.start = $('#datetimepicker1 > input').val();
+            //vm.event.start = moment.utc($("#datetimepicker1").data("DateTimePicker").date()).valueOf();
+            vm.event.start = $('#datetimepicker1 > input').val();
         });
       
     vm.onSubmit = function () {
         console.log('Submitting events ' + vm.event.title);
+        vm.event.start = moment.utc($("#datetimepicker1").data("DateTimePicker").date()).valueOf();
         meanData.saveEvent(vm.event)
             .error(function(e){
                 console.log(e);
