c4d2e970f9fd82960b724afa7c0cb430a86054f0
Seth Anderson <seth.c.anderson@gmail.com>
2016-07-12 04:03:27 -0500

    modal calendar events

--- app_client/calendar/calendar.controller.js
+++ app_client/calendar/calendar.controller.js
@@ -13,23 +13,42 @@
         .success(function(data) {
             $('#calendar').fullCalendar({
                 events: data,
-                timezone: 'local'
+                timezone: 'local',
+                eventClick: function(event, jsEvent, view) {
+                    $('#modalTitle').html(event.title);
+                    $('#eventtitle').val(event.title);
+                    $('#eventstart').val(event.start);
+                    $('#eventend').val(event.end);
+//                    $('#modalBody').html(event.description);
+//                    $('#eventUrl').attr('href',event.url);
+                    $('#fullCalModal').modal();
+                }
             });
         })
         .error(function (e) {
             console.log(e);
         });
       
-    $('#datetimepicker1')
+    $('#neweventbutton').click(function(){
+        $('#neweventmodal').modal();
+    });
+    
+    $('#starttimepicker')
         .datetimepicker()
         .on("dp.change", function() {
-            //vm.event.start = moment.utc($("#datetimepicker1").data("DateTimePicker").date()).valueOf();
-            vm.event.start = $('#datetimepicker1 > input').val();
+            vm.event.start = $('#starttimepicker > input').val();
+        });
+      
+    $('#endtimepicker')
+        .datetimepicker()
+        .on("dp.change", function() {
+            vm.event.start = $('#endtimepicker > input').val();
         });
       
     vm.onSubmit = function () {
         console.log('Submitting events ' + vm.event.title);
-        vm.event.start = moment.utc($("#datetimepicker1").data("DateTimePicker").date()).valueOf();
+        vm.event.start = moment.utc($("#starttimepicker").data("DateTimePicker").date()).valueOf();
+        vm.event.end = moment.utc($("#endtimepicker").data("DateTimePicker").date()).valueOf();
         meanData.saveEvent(vm.event)
             .error(function(e){
                 console.log(e);
