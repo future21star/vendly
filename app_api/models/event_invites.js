f61ac39d1d216c76c1c3c8bd1bb7e12856134a0b
Seth Anderson <seth@vendly.com>
2017-01-06 19:00:38 -0600

    adding for git migration

--- app_api/models/event_invites.js
+++ app_api/models/event_invites.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
-var Contact = mongoose.model('Contact');
+var User = mongoose.model('User');
 var Event = mongoose.model('Event');
 
 var eventInviteSchema = new mongoose.Schema({
     accepted: String,
     event: { type: mongoose.Schema.Types.ObjectId, ref: 'Event' },
-    guest: { type: mongoose.Schema.Types.ObjectId, ref: 'Contact' }
+    guest: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
 
 mongoose.model('EventInvite', eventInviteSchema);
