7a1206853a2eefcf586741b4f7367974c92e0d92
Adam Inglehart <adam.inglehart@gmail.com>
2017-03-16 19:52:17 -0700

    Split services from main.js into separate files

---
+++ app_client/js/services/authenticationService.js
@@ -0,0 +1,83 @@
+/**
+ * Created by Adam on 3/16/17.
+ */
+var app = angular.module("MetronicApp");
+
+app.service('authentication', ['$http', '$window',
+    function($http, $window) {
+
+        var saveToken = function(token) {
+            $window.localStorage['mean-token'] = token;
+        };
+
+        var clearToken = function() {
+            $window.localStorage.removeItem('mean-token');
+        };
+
+        var getToken = function() {
+            return $window.localStorage['mean-token'];
+        };
+
+        var isLoggedIn = function() {
+            var token = getToken();
+            var payload;
+
+            if (token) {
+                payload = token.split('.')[1];
+                payload = $window.atob(payload);
+                payload = JSON.parse(payload);
+
+                return payload.exp > Date.now() / 1000;
+            } else {
+                return false;
+            }
+        };
+
+        var currentUser = function() {
+            if (isLoggedIn()) {
+                var token = getToken();
+                var payload = token.split('.')[1];
+                payload = $window.atob(payload);
+                payload = JSON.parse(payload);
+                return {
+                    email: payload.email,
+                    name: payload.name
+                };
+            }
+        };
+
+        var register = function(user) {
+            console.log(`User: ${JSON.stringify(user)}`);
+            return $http.post('/register', user).success(function(data) {
+                saveToken(data.token);
+            });
+        };
+
+        var login = function(user) {
+            return $http.post('/api/login', user).success(function(data) {
+                saveToken(data.token);
+            });
+        };
+
+        var logout = function() {
+            clearToken();
+        };
+
+        var changePassword = function() {
+            return $http.post('/api/changePassword', user).success(function(data) {
+                saveToken(data.token);
+            });
+        };
+
+        return {
+            currentUser: currentUser,
+            saveToken: saveToken,
+            getToken: getToken,
+            isLoggedIn: isLoggedIn,
+            register: register,
+            login: login,
+            logout: logout,
+            changePassword: changePassword
+        };
+    }
+]);
