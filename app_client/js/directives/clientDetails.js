f6a624f8d38422bb993dc5ed860acf30d8be6ac1
Adam Inglehart <adam.inglehart@gmail.com>
2017-03-22 21:42:52 -0700

    Modified client details directive to contain all the modal content (including header and footer) and continued building edit functionality

--- app_client/js/directives/clientDetails.js
+++ app_client/js/directives/clientDetails.js
@@ -1,13 +1,29 @@
 /**
  * Created by Adam on 3/16/17.
  */
-app = angular.module("MetronicApp")
+app = angular.module("MetronicApp");
 
-app.directive('clientDetails', function() {
-   return {
-       templateUrl: "/views/templates/client-details.html",
-       scope: {
-           client: "=client"
-       }
-   }
+app.directive("clientDetails", function () {
+  return {
+    templateUrl: "/views/templates/client-details.html",
+    scope: {
+      client: "=client",
+    },
+    controller: ["$scope", function ($scope) {
+
+      $scope.edit = function () {
+        $scope.editedClient = angular.copy($scope.client);
+        $scope.editing = true
+      };
+
+      $scope.stopEditing = function() {
+        $scope.editing = false
+      };
+
+      $scope.save = function() {
+
+      };
+
+    }]
+  }
 });
