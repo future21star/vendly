b0ed31fbada5674354afe1b688b85b4ad8c4e41e
Adam Inglehart <adam.inglehart@gmail.com>
2017-04-05 20:25:08 -0700

    Use a method to navigate to client details page to get rid of modal backdrop (there has to be a better way)

--- app_client/js/directives/clientDetails.js
+++ app_client/js/directives/clientDetails.js
@@ -9,26 +9,33 @@ app.directive("clientDetails", function () {
     scope: {
       client: "=client",
     },
-    controller: ["$scope", "meanData", function ($scope, meanData) {
+    controller: ["$scope", "meanData", "$location", function ($scope, meanData, $location) {
 
       $scope.edit = function () {
         $scope.editedClient = angular.copy($scope.client);
         $scope.editing = true
       };
 
-      $scope.stopEditing = function() {
+      $scope.stopEditing = function () {
         $scope.editing = false
       };
 
-      $scope.save = function() {
-        $scope.error = null
-        meanData.updateContact($scope.editedClient, function(response) {
+      $scope.save = function () {
+        $scope.error = null;
+        meanData.updateContact($scope.editedClient, function (response) {
           $scope.client = response.data;
           $scope.stopEditing()
-        }, function(response) {
+        }, function (response) {
           $scope.error = response.data.message;
         })
       };
+
+      $scope.goToClientDetails = function () {
+        // TEMP to get rid of modal backdrop div
+        $location.path("/client_details");
+        $location.search("clientId", $scope.client._id);
+        $('.modal-backdrop').remove();
+      }
 
     }]
   }
