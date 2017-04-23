fe3bf5fad784489c8abcf1b7881fbf4d4b363329
Adam Inglehart <adam.inglehart@gmail.com>
2017-04-23 16:42:21 -0700

    Created handbook directive and used it in newhandbook.html and handbook_editor.html (still needs save functionality)

--- app_client/js/controllers/HandbookController.js
+++ app_client/js/controllers/HandbookController.js
@@ -9,38 +9,15 @@ angular.module('MetronicApp').controller('HandbookController', ['$rootScope', '$
     $rootScope.settings.layout.pageSidebarClosed = false;
   });
 
-  $scope.content = [
-    {
-      "label": "Section Break", "field_type": "new_section_break", "required": false,
-      "field_options": {"description": "Description about this section."}, "cid": "c8"
-    },
-    {
-      "label": "Wedding Date",
-      "field_type": "date", "required": true, "field_options": {"description": ""}, "cid": "c13"
-    },
-    {
-      "label": "Which if the following do you want to include in your wedding?", "field_type": "checkboxes",
-      "required": true, "field_options": {
-      "options": [
-        {"label": "Unity Candle", "checked": false},
-        {"label": "Wedding cord/lasso", "checked": false}], "include_other_option": true
-    }, "cid": "c17"
-    }
-  ];
+  if ($stateParams.hasOwnProperty('handbookId')) {
+    $scope.handbookId = $stateParams.handbookId
+  }
 
-  $scope.handbook = {};
   $scope.handbooks = {};
   var loadHandbooks = function () {
     meanData.getBooklets()
       .success(function (data) {
         $scope.handbooks = data;
-        if ($stateParams.hasOwnProperty('handbookId')) {
-          $scope.handbook = $.grep(data, function (e) {
-            return e._id == $stateParams.handbookId
-          })[0];
-
-          $scope.content = $scope.handbook.content.fields;
-        }
       })
       .error(function (e) {
         console.log(e);
